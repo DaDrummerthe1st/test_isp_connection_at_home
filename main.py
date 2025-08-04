6b226920eb4e88741c06e449e467ca4685746570
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-08-04 15:00:36 +0200

    relying on cron. not working yet

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import time
+from time import sleep
 import sqlite3
 from datetime import datetime
 import subprocess
+import random
+from pathlib import Path
 
-DB_FILE = "ping_results.db"
+HOMEDIR = Path.home()
+DB_FILE = f"{HOMEDIR}/check_internet/test_isp_connection_at_home/ping_results.db"
+
+respondents = [
+    "ping.sunet.se",
+    "aftonbladet.se",
+    "ping.bahnhof.se",
+    "netgear.com",
+    "8.8.8.8",
+    "bbc.co.uk",
+    "192.0.43.10",
+    "4.2.2.2"
+]
 
 def init_db():
     conn = sqlite3.connect(DB_FILE)
@@ -13,18 +27,19 @@ def init_db():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             timestamp TEXT NOT NULL,
             success INTEGER NOT NULL,
-            rtt_ms REAL
+            rtt_ms FLOAT
+	        respondents TEXT NOT NULL
         )
     """)
     conn.commit()
     conn.close()
 
-def log_ping(success, rtt_ms):
+def log_ping(success, rtt_ms, respondent):
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     c.execute(
-        "INSERT INTO pings (timestamp, success, rtt_ms) VALUES (?, ?, ?)",
-        (datetime.now().isoformat(), int(success), rtt_ms)
+        "INSERT INTO pings (timestamp, success, rtt_ms,respondent) VALUES (?, ?, ?, ?)",
+        (datetime.now().isoformat(), int(success), rtt_ms, respondent)
     )
     conn.commit()
     conn.close()
@@ -46,17 +61,21 @@ def ping_host(host):
                 if "time=" in line:
                     rtt_ms = float(line.split("time=")[-1].split()[0])
                     break
-        return success, rtt_ms
+        return success, rtt_ms, host
     except Exception:
-        return False, None
+        return False, None, host
 
 def main():
     init_db()
     while True:
-        success, rtt_ms = ping_host('sunet.se')
-        log_ping(success, rtt_ms)
-        print(f"{datetime.now().isoformat()} | Success: {success} | RTT: {rtt_ms}")
-        time.sleep(30)  # not 5 minutes
+        random_respondent = respondents[random.randint(0,len(respondents)-1)]
+        success, rtt_ms, respondent = ping_host(random_respondent)
+        log_ping(success, rtt_ms, respondent)
+        print(f"{datetime.now().isoformat()} | Success: {success} | RTT: {rtt_ms} | {respondent}")
+        # if success:
+        #     sleep(60)
+        # else:
+        #     sleep(20)
 
 if __name__ == "__main__":
     main()
---
+++ testcron.py
@@ -0,0 +1,1 @@
+print("I will probably never see this")
